3956cc4c20bd42cba98c320af4075c90e1c734d8
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-05-15 13:09:53 +0530

    updated link

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var userInput = document.querySelector(".txt_input");
 var btnTranslate = document.querySelector("#btn_input");
 var userOutput = document.querySelector(".txt_output");
 
-var url = "https://api.funtranslations.com/translate/groot.json"
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 function generateUrl(text) {
     return url+"?text="+text;
